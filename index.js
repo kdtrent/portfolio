6f7b8da3459d727549317da6ca343b9165287b51
kdtrent <kalebtrent08@gmail.com>
2025-06-29 18:34:19 -0400

    attempting to fix drop down menu

--- index.js
+++ index.js
@@ -6,3 +6,18 @@ function activateButton(button) {
     // Add "active" class to the clicked button
     button.classList.add('active');
   }
+
+  var dropdown = document.getElementsByClassName("dropdown-btn");
+  var i;
+
+  for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var dropdownContent = this.nextElementSibling;
+      if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+      } else {
+        dropdownContent.style.display = "block";
+      }
+    });
+  }
