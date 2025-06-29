768a8f3e68dded075f970450546af647aa7c4de4
kdtrent <kalebtrent08@gmail.com>
2025-06-29 19:14:24 -0400

    Same this as last message

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 function activateButton(button) {
-    // Remove "active" class from all buttons
-    const buttons = document.querySelectorAll('.sidenav a', '.sidenav button');
-    buttons.forEach(btn => btn.classList.remove('active'));
+  // Remove "active" class from all buttons
+  const buttons = document.querySelectorAll('.sidenav a', '.sidenav button');
+  buttons.forEach(btn => btn.classList.remove('active'));
 
-    // Add "active" class to the clicked button
-    button.classList.add('active');
-  }
+  // Add "active" class to the clicked button
+  button.classList.add('active');
+}
 
   var dropdown = document.getElementsByClassName("dropdown-btn");
   var i;
 
-  for (i = 0; i < dropdown.length; i++) {
-    dropdown[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      var dropdownContent = this.nextElementSibling;
-      if (dropdownContent.style.display === "block") {
-        dropdownContent.style.display = "none";
-        activateButton(button);
-      } else {
-        dropdownContent.style.display = "block";
-      }
-    });
-  }
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    activateButton(button);
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
