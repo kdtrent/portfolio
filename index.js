e22695a203a17a7e8b50c20b7d9a30446808676f
kdtrent <kalebtrent08@gmail.com>
2025-06-29 15:35:49 -0400

    Trying to fix the arrow now

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function activateButton(button) {
     // Remove "active" class from all buttons
-    const buttons = document.querySelectorAll('.sidenav a');
+    const buttons = document.querySelectorAll('.sidenav a', '.sidenav dropdown-btn');
     buttons.forEach(btn => btn.classList.remove('active'));
 
     // Add "active" class to the clicked button
