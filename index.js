f4414b2f0b5524534fc90bcee12ddbc697354121
kdtrent <kalebtrent08@gmail.com>
2025-06-29 19:21:45 -0400

    testing

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 function activateButton(button) {
-  // Remove "active" class from all buttons
-  const buttons = document.querySelectorAll('.sidenav a', '.sidenav button');
-  buttons.forEach(btn => btn.classList.remove('active'));
+    // Remove "active" class from all buttons
+    const buttons = document.querySelectorAll('.sidenav a', '.sidenav button');
+    buttons.forEach(btn => btn.classList.remove('active'));
 
-  // Add "active" class to the clicked button
-  button.classList.add('active');
-}
+    // Add "active" class to the clicked button
+    button.classList.add('active');
+  }
 
   var dropdown = document.getElementsByClassName("dropdown-btn");
   var i;
 
-for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var dropdownContent = this.nextElementSibling;
-    if (dropdownContent.style.display === "block") {
-      dropdownContent.style.display = "none";
-    } else {
-      dropdownContent.style.display = "block";
-    }
-  });
-}
+  for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var dropdownContent = this.nextElementSibling;
+      if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+      } else {
+        dropdownContent.style.display = "block";
+      }
+    });
+  }
