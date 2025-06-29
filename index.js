294e3d4d8d306599fb3ccdf759146b3f9db4812f
kdtrent <kalebtrent08@gmail.com>
2025-06-29 19:18:38 -0400

    everything broke trying to find culprit

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ for (i = 0; i < dropdown.length; i++) {
   dropdown[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var dropdownContent = this.nextElementSibling;
-    activateButton(button);
     if (dropdownContent.style.display === "block") {
       dropdownContent.style.display = "none";
     } else {
