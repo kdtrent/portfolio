d39aa4c34df608161527819f22ecd01d9696396a
kdtrent <kalebtrent08@gmail.com>
2025-06-29 20:08:10 -0400

    No issues thus far. Added more to see the breaking point

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function activateButton(button) {
     // Remove "active" class from all buttons
-    const buttons = document.querySelectorAll('.sidenav a', '.sidenav button');
+    const buttons = document.querySelectorAll('.sidenav a', '.sidenav dropdown-btn');
     buttons.forEach(btn => btn.classList.remove('active'));
 
     // Add "active" class to the clicked button
